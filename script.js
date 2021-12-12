9713dba57d4947c13a258d65cd9911afae10fc8e
Cloud Vlad <v.nikolov.nn@gmail.com>
2021-12-12 20:55:59 +0200

    Basics of the game - done.

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-var SCALE = 1;
-var SPEED = 1;
+
+
+var timeoutId = null;
+
 var matrix;
 
 var table = document.getElementById("grid-table");
 var start_button = document.getElementById("start-button");
 var status = false;
 
-
-
-var maxWidth = (Math.floor((window.innerWidth - 20) / 20)) / SCALE;
-var maxHeight = (Math.floor((window.innerHeight - 20) / 20)) / SCALE;
-var maxHeight = (maxHeight - 5) / SCALE;
+var maxWidth = (Math.floor((window.innerWidth - 20) / 20));
+var maxHeight = (Math.floor((window.innerHeight - 20) / 20));
+var maxHeight = (maxHeight - 5);
 var colorChange = function () {
     cell_set(this);
 };
+
+function draw() {
+    for (let i = 0; i < maxHeight; i++) {
+        var row = table.insertRow(i);
+        for (let j = 0; j < maxWidth; j++) {
+            var cell = row.insertCell(j);
+            cell.setAttribute("id", (i.toString() + "-" + j.toString()));
+            cell.addEventListener("click", this.colorChange, false);
+        }
+    }
+}
 
 window.onload = function () {
     draw();
@@ -27,52 +38,52 @@ window.onload = function () {
 function cell_set(el) {
     var x = parseInt(el.id.split("-")[0]);
     var y = parseInt(el.id.split("-")[1]);
+    if (matrix[x][y])
+    {
+        matrix[x][y] = false;
+        el.style.backgroundColor = "#E7E7E7";
+        return;
+    }
     matrix[x][y] = true;
     el.style.backgroundColor = "black";
 }
 
-function draw() {
-    for (let i = 0; i < maxHeight; i++) {
-        var row = table.insertRow(i);
-        for (let j = 0; j < maxWidth; j++) {
-            var cell = row.insertCell(j);
-            cell.setAttribute("id", (i.toString() + "-" + j.toString()));
-            cell.addEventListener("click", this.colorChange, false);
-        }
-    }
-}
+
 
 
 
 
 function deadOrAlive(x, y) {
-    var xmap = [-1, 0, 1, 1, 1, 0, -1, -1];
-    var ymap = [1, 1, 1, 0, -1, -1, -1, 0];
+    // console.log(x + "-" + y);
+    var xmap = [0, 1, 1, 1, 0, -1, -1, -1];
+    var ymap = [-1, -1, 0, 1, 1, 1, 0, -1];
 
     var aliveCells = 0;
     for (let i = 0; i < 8; i++) {
-        if (x + xmap[i] < 0 || y + ymap[i] < 0)
+        // console.log(i);
+        if ((x + xmap[i]) < 0 || (y + ymap[i]) < 0 || (x + xmap[i]) == maxHeight || (y + ymap[i]) == maxWidth)
         {
             continue;
         }
 
+        
         if (matrix[x + xmap[i]][y + ymap[i]]) {
             aliveCells += 1;
         }
     }
     // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-    if (aliveCells < 2)
+    if (matrix[x][y] && aliveCells < 2)
     {
         return false;
     }
 
     // Any live cell with two or three live neighbours lives on to the next generation.
-    if (aliveCells == 2 || aliveCells == 3) {
+    if (matrix[x][y] && (aliveCells == 2 || aliveCells == 3)) {
         return true;
     }
 
     // Any live cell with more than three live neighbours dies, as if by overpopulation.
-    if (aliveCells > 3) {
+    if (matrix[x][y] && aliveCells > 3) {
         return false;
     }
 
@@ -101,21 +112,23 @@ function calculate() {
 }
 
 function update() {
-    for (let i = 0; i < maxHeight; i++) {
-        for (let j = 0; j < maxWidth; j++) {
+    calculate();
+    for (var i = 0; i < maxHeight; i++) {
+        for (var j = 0; j < maxWidth; j++) {
+            var cell = document.getElementById(i + "-" + j);
             if (matrix[i][j]) {
-                document.getElementById(toString(i) + "-" + toString(j)).style.backgroundColor = "black";
+                cell.style.backgroundColor = "black";
             }
             else {
-                document.getElementById(toString(i) + "-" + toString(j)).style.backgroundColor = "white";
+                cell.style.backgroundColor = "#E7E7E7";
             }
         }
     }
+    timeoutId = setTimeout(update, 1000);
 }
 
 function execute() {
-    calculate();
-    draw();
+    update();
 }
 
-start_button.addEventListener("click", execute(), false);
+start_button.addEventListener("click", function () {execute();}, false);
